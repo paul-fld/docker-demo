3b1b5806f426b8dc8bf55836303ccf18bd957f02
Paul <paul.flaud@efrei.net>
2026-06-29 11:47:19 +0200

    Initial commit - Spring Boot Hello World + REST controller

---
+++ src/main/java/com/example/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+}
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
+
